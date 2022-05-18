2d0dd2329129518ad50f59dcf4f886435e4c2c7f
Emrecan Balgün <deathlygrave@gmail.com>
2022-05-18 23:16:20 +0300

    update app.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [value, setValue] = useState(1000);
+  const minValue = 200;
+  const maxValue = 5000;
+  
   return (
     <div className="App">
         <div className="container">
@@ -11,6 +16,15 @@ function App() {
           <hr className="hr"/>
           <div className="bahis-slider">
               <span>Bahis Aralığı</span>
+              <span className="value">Min {minValue}</span>
+              <input type="range" min={minValue} max={maxValue} value={value}/>
+              <span className="value">Max {maxValue}</span>
+          </div>
+          <div className="filters">
+              <div className="filter">
+                <span>Hızlı</span>
+                <input type="radio" name="range" />
+              </div>
           </div>
           <hr />
         </div>
