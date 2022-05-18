cd01c0616e8148f14993e2ec42f26f6dfc821be6
Emrecan Balgün <deathlygrave@gmail.com>
2022-05-18 22:32:49 +0300

    update app.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <div className="container">
+          <div className="header">
+             <span className="header-text">Masa Özelliklerine Geç</span>
+             <span className="header-text">X</span>
+          </div>
+          <hr className="hr"/>
+          <div className="bahis-slider">
+              <span>Bahis Aralığı</span>
+          </div>
+          <hr />
+        </div>
     </div>
   );
 }
